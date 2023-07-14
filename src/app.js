933990569186b5b3d354c6821a86a93bec3daac5
Shubham <shubhamdesai20003@gmail.com>
2023-07-14 16:50:21 +0530

    import_app.js

--- src/app.js
+++ src/app.js
@@ -1,37 +1,42 @@
-console.log("done");
-
-$(document).ready(function(){
-  $('.food-slider').slick({
-    autoplay: true,
-    slidesToScroll: 1,
-    slidesToShow:3,
-    prevArrow:".prev-btn",
-    nextArrow:".next-btn",
-    responsive:[
-      {
-        breakpoint: 1640,
-        settings:{
-          slidesToShow: 3,
+const card = ()=>{
+  console.log("done");
+  
+  $(document).ready(function(){
+    $('.food-slider').slick({
+      autoplay: true,
+      slidesToScroll: 1,
+      slidesToShow:3,
+      prevArrow:".prev-btn",
+      nextArrow:".next-btn",
+      responsive:[
+        {
+          breakpoint: 1640,
+          settings:{
+            slidesToShow: 3,
+          }
+        },
+        {
+          breakpoint: 992,
+          settings:{
+            slidesToShow: 2,
+          }
+        },
+        {
+          breakpoint: 768,
+          settings: {
+          slidesToShow: 1,
+          }
         }
-      },
-      {
-        breakpoint: 992,
-        settings:{
-          slidesToShow: 2,
-        }
-      },
-      {
-        breakpoint: 768,
-        settings: {
-        slidesToShow: 1,
-        }
-      }
-      ]
+        ]
+      
+    });
+    
+    $('.nav-trigger').click(function(){
+      $('.site-content-wrapper').toggleClass('scaled');
+    })
     
   });
-  
-  $('.nav-trigger').click(function(){
-    $('.site-content-wrapper').toggleClass('scaled');
-  })
-  
-});
+
+}
+
+export default card;
--- src/main.js
+++ src/main.js
@@ -11,42 +11,6 @@ import '../styles/components/contact-us.css';
 import '../styles/components/footer.css';
 import '../styles/components/mobile-nav.css';
 
+import card from './app';
 
-
-console.log("done");
-
-$(document).ready(function(){
-  $('.food-slider').slick({
-    autoplay: true,
-    slidesToScroll: 1,
-    slidesToShow:3,
-    prevArrow:".prev-btn",
-    nextArrow:".next-btn",
-    responsive:[
-      {
-        breakpoint: 1640,
-        settings:{
-          slidesToShow: 3,
-        }
-      },
-      {
-        breakpoint: 992,
-        settings:{
-          slidesToShow: 2,
-        }
-      },
-      {
-        breakpoint: 768,
-        settings: {
-        slidesToShow: 1,
-        }
-      }
-      ]
-    
-  });
-  
-  $('.nav-trigger').click(function(){
-    $('.site-content-wrapper').toggleClass('scaled');
-  })
-  
-});
+card();
