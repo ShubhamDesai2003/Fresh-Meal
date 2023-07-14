47220093f530099be18d43e847329e3ab92890a3
Shubham <shubhamdesai20003@gmail.com>
2023-07-14 11:52:23 +0530

    first commit

---
+++ src/main.js
@@ -0,0 +1,26 @@
+import '../styles/style.css';
+import '../styles/components/site-content.css';
+import '../styles/components/latest-news.css';
+import '../styles/components/hero.css';
+import '../styles/components/top-products.css';
+import '../styles/components/about-meal.css';
+import '../styles/components/our-services.css';
+import '../styles/components/big-deal.css';
+import '../styles/components/subscribe.css';
+import '../styles/components/contact-us.css';
+import '../styles/components/footer.css';
+import '../styles/components/mobile-nav.css';
+
+
+
+/* @media(max-width:992px){
+
+} */
+
+/* @media(max-width:768px){
+
+} */
+
+/* @media(max-width: 576px){
+    
+} */
